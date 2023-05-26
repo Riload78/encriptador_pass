6d70af5a93f3d497e0f9c7c549eb33df164b149f
Riload <ricardovlmmarcos@gmail.com>
2023-05-26 07:36:21 +0200

    Hash_pass modify

--- encriptador.py
+++ encriptador.py
@@ -1,17 +1,17 @@
 import random
 from werkzeug.security import generate_password_hash
 
-class Hash_pass():
-    minus = 'abcdefghijklmnopqrstuvwxyz'
-    mayus = minus.upper()
-    num = '0123456789'
-    simb = '!@#$%&/¡¿+*'
-    longitud = 0
-    custom_pass = ''
-    
-    
-    def __init__(self,longitud):
-        self.longitud = longitud
+class Hash_pass:
+
+    def __init__(self):
+        self.custom_pass = ''
+        self.minus = 'abcdefghijklmnopqrstuvwxyz'
+        self.mayus = self.minus.upper()
+        self.num = '0123456789'
+        self.simb = '!@#$%&/¡¿+*'
+        self.longitud = 12
+        
+        
         
     def __str__(self) -> str:
             return (f'minus:{self.minus},mayus:{self.mayus},num:{self.num},simb:{self.simb},longitud:{self.longitud}')
@@ -30,8 +30,9 @@ class Hash_pass():
         hash_pass = generate_password_hash(custom_pass)
         return (f'custom_pass:{custom_pass} || encript:{hash_pass}')
 
-new_hash = Hash_pass(12)
+new_hash = Hash_pass()
 print(new_hash)
 
 print(new_hash.get_random_hash(8))
 print(new_hash.get_hash('123asdzxc!'))
+print(new_hash.longitud)
